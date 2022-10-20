5c6f9983c9172fdc1d89d4de2cec5e906259359e
sashalarrieu <sasha.larrieu@epsi.fr>
2022-10-20 15:03:15 +0200

    petit test swaggi

--- BowlingGame.Test/UnitTest1.cs
+++ BowlingGame.Test/UnitTest1.cs
@@ -6,8 +6,110 @@ namespace BowlingGame.Test
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test2Quilles()
         {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 2 quilles
+            partie.Lancer(2);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(0, partie.Score);
+        }
+
+
+        [TestMethod]
+        public void Test2Lancers1Quille()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 1 quilles deux fois
+            partie.Lancer(1);
+            partie.Lancer(1);
+
+            // ALORS le score est de 2
+            Assert.AreEqual(2, partie.Score);
+        }
+
+        [TestMethod]
+        public void Test1Quille()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber une quille
+            partie.Lancer(1);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(0, partie.Score);
+        }
+
+        [TestMethod]
+        public void TestLancerRaté()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 0 quilles
+            partie.Lancer(0);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(0, partie.Score);
+        }
+
+        [TestMethod]
+        public void TestStrike()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 10 quilles
+            partie.Lancer(10);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(0, partie.Score);
+        }
+
+        [TestMethod]
+        public void TestSpare8Quilles()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 10 quilles en 2 fois
+            partie.Lancer(9);
+            partie.Lancer(1);
+
+            partie.Lancer(8);
+            partie.Lancer(1);
+
+            // ALORS le score est de 0
+            Assert.AreEqual(27, partie.Score);
+        }
+
+        [TestMethod]
+        public void TestSpare3Quilles()
+        {
+            // ETANT DONNE une partie
+            var partie = new Partie();
+
+            // QUAND on fait tomber 10 quilles en 2 fois
+            partie.Lancer(9);
+            partie.Lancer(1);
+            //Score = 10
+
+            partie.Lancer(3);
+            //Score = 13
+
+            partie.Lancer(2);
+            //Score = 15
+
+            //Avec bonus Score = 18
+
+            // ALORS le score est de 0
+            Assert.AreEqual(18, partie.Score);
         }
     }
 }
---
+++ BowlingGame/Partie.cs
@@ -0,0 +1,34 @@
+﻿namespace BowlingGame;
+
+public class Partie
+{
+    private readonly List<int> _historique = new ();
+
+    public void Lancer(int nombreQuillesTombées)
+    {
+        _historique.Add(nombreQuillesTombées);
+
+        if(EstUnStrike()) return;
+        if (EstUnSpare())
+        {
+            ScoreSpare = DeuxDerniersLancers.Sum();
+        }
+
+        if (TermineUneFrame())
+        {
+            Score = DeuxDerniersLancers.Sum();
+            if (ScoreSpare == 10)
+            {
+                Score = Score + PremierDeuxDerniersLancers + ScoreSpare;
+            }
+        }
+    }
+    private IEnumerable<int> DeuxDerniersLancers => _historique.AsEnumerable().Reverse().Take(2);
+    private int PremierDeuxDerniersLancers => DeuxDerniersLancers.Reverse().First();
+    private bool TermineUneFrame() => _historique.Count % 2 == 0;
+    private bool EstUnSpare() => DeuxDerniersLancers.Sum() == 10;
+    private bool EstUnStrike() => _historique.Last() == 10;
+
+    public int Score { get; private set; }
+    public int ScoreSpare { get; private set; }
+}
